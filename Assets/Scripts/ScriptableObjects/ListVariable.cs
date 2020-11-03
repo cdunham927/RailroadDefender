150b58cdcbe7b5c108a47975118f3ccdd2b6796c
cdunham927 <cdunham927@gmail.com>
2020-11-03 00:43:58 -0800

    Base game with some starter scripts that don't do much of anything yet

---
+++ Assets/Scripts/ScriptableObjects/ListVariable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ListVariable<T> : ScriptableObject
+{
+    public List<T> Items = new List<T>();
+    public void Add(T t)
+    {
+        if (!Items.Contains(t)) Items.Add(t);
+    }
+
+    public void Remove(T t)
+    {
+        if (Items.Contains(t)) Items.Remove(t);
+    }
+}
