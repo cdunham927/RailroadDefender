6e793d3d8d9e2d0dfc7e7177fa6476f8eef5fc43
cdunham927 <cdunham927@gmail.com>
2020-11-12 13:46:15 -0800

    Third person character controller(kinda)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public FloatVariable followSpeed;
+    //Might need variable for switch speed
+
+    Vector3 startPos;
+    public Transform target;
+
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, startPos + target.transform.position, Time.fixedDeltaTime * followSpeed.Value);
+    }
+
+    public void SetTarget(Transform tar)
+    {
+        target = tar;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,76 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour, IDamageable<float>, IKillable
 {
+    //Stats
     public FloatVariable maxHp;
     float hp;
+    public FloatVariable speed;
+    public FloatVariable runSpeed;
+    float curSpeed;
+
+    //Input
+    Vector2 inp;
+
+    //Components
+    //Rigidbody bod;
+    Vector3 mousePos;
+    CharacterController controller;
+
+    //Movement and rotation
+    Vector3 movement;
+    float rotationX = 0;
+    public float lookXLimit = 45.0f;
+    public float jumpHeight = 8f;
+    public float grav = 20f;
+    public float rotSpd;
 
     void OnEnable()
     {
+        //bod = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
         hp = maxHp.Value;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        //if (controller == null) controller = GetComponent<CharacterController>();
+        mousePos = Input.mousePosition;
+        inp = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        //Change run speed if you're holding the run button down
+        curSpeed = Input.GetButton("Run") ? runSpeed.Value : speed.Value;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float movementY = movement.y;
+
+        movement = (forward * curSpeed * inp.y) + (right * curSpeed * inp.x);
+
+        //Jumping
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            movement.y = jumpHeight;
+        }
+        else
+        {
+            movement.y = movementY;
+        }
+
+        if (!controller.isGrounded)
+        {
+            movement.y -= grav * Time.deltaTime;
+        }
+
+        if (controller.enabled) controller.Move(movement * Time.deltaTime);
+
+        //Rotation
+        rotationX += -Input.GetAxis("Mouse Y") * rotSpd;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * rotSpd, 0);
     }
 
     public void Kill()
