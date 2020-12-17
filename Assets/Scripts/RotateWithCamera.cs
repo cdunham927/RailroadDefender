c0572ef460e711774f33bf5b96b0b5905e42ca30
cdunham927 <cdunham927@gmail.com>
2020-12-17 13:06:43 -0800

    Imported assets, started player and gun stuff

---
+++ Assets/Scripts/RotateWithCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithCamera : MonoBehaviour
+{
+    float rotationX = 0f;
+    PlayerController player;
+    Quaternion startRot;
+
+    private void Awake()
+    {
+        startRot = transform.localRotation;
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            //Rotation
+            rotationX += -Input.GetAxis("Mouse Y") * player.rotSpd;
+            rotationX = Mathf.Clamp(rotationX, -player.lookXLimit, player.lookXLimit);
+            transform.localRotation = startRot * Quaternion.Euler(-rotationX, 0, 0);
+            //transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * player.rotSpd, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/ProjectileController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public float dmg;
+    Rigidbody bod;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+}
