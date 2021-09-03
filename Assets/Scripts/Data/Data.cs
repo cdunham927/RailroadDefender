0885f2d3504d106577cafc5d481a9fb63b555db8
cdunham927 <cdunham927@gmail.com>
2021-09-03 10:42:54 -0700

    Started save data

---
+++ Assets/Scripts/Data/Data.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data
+{
+    public bool easyMode;
+    public int sceneNumber;
+    //Save info about currently completed levels, weapons unlocked, etc etc
+    //player position
+    public float[] playerPosition;
+
+    //Might need music and sound volume in here too
+    //
+    //
+    //
+    //
+
+    public Data(bool em = true, int sceneNum = 0, float xPos = 0, float yPos = 0)
+    {
+        //Easy mode on or off
+        easyMode = em;
+        //Scene number
+        sceneNumber = sceneNum;
+        //New float array
+        playerPosition = new float[2];
+        playerPosition[0] = xPos;
+        playerPosition[1] = yPos;
+    }
+}
---
+++ Assets/Scripts/Data/SaveSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem
+{
+    public static void SaveData(bool em = false, int sceneNum = 0, float xPos = 0, float yPos = 0)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/data";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        Data data = new Data(em, sceneNum, xPos, yPos);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static Data LoadData()
+    {
+        string path = Application.persistentDataPath + "/data";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            Data data = formatter.Deserialize(stream) as Data;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("No save data found in " + path);
+            return null;
+        }
+    }
+}
